c4a86e48007712a55e96afd85b4c649b37289d14
bradley <fazebrad13@gmail.com>
2025-10-23 15:17:40 +0300

    last commit

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ErrorPage() {
+  return (
+    <div>
+      <h1>Oops! Page not found.</h1>
+    </div>
+  );
+}
+
+export default ErrorPage;
+
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,22 @@
-
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Movie from "./components/Movie";
+import Home from "./components/Home";
+import Actors from "./components/Actors";
+import Directors from "./components/Directors";
 
 const routes = [
-   
-  ];
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "movie/:id", element: <Movie /> },
+      { path: "actors", element: <Actors /> },
+      { path: "directors", element: <Directors /> },
+    ],
+  },
+];
 
 export default routes;
